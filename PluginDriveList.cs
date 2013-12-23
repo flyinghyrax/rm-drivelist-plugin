64254a4e911831520954eb7a685a1a0f0c172af5
Matthew Seiler <theflyinghyrax@gmail.com>
2013-12-23 15:26:00 -0500

    Drive names are now in form "C:" instead of "C:\". Finished implementing ErrorString.  Set version to something more sensible.  General code cleanup, addded lots of comments, updated README.

--- PluginDriveList.cs
+++ PluginDriveList.cs
@@ -1,4 +1,22 @@
-﻿using System;
+﻿/*
+  Copyright (C) 2013 Matthew Seiler
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+using System;
 using System.IO;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
@@ -8,11 +26,17 @@ namespace PluginDriveList
 {
     internal class Measure
     {
+        // Dict to track drive type settings
         private Dictionary<DriveType, bool> listedTypes;
+        // GetString() returns this value on error
         private string ErrorString;
+        // Array of DriveInfo objects representing all connected drives
         private DriveInfo[] allDrives;
+        // List of drive letters that we can return
         private List<string> listedDrives;
+        // Index of the "current" element in the above list.
         private int currentIndex;
+        // Flag indicating whether or not to update listedDrives this cycle.
         private bool needUpdate;
 
         /* Measure object constructor initializes class fields
@@ -34,21 +58,33 @@ namespace PluginDriveList
             needUpdate = true;
         }
 
+        /* Runs on load/refresh on on every update cycle if DynamicVariables is set.
+         * Reads error string and drive type settings.  For DynamicVariables - if
+         * the type settings dictionary from this reload is different from that of the 
+         * last reload, then set the update flag.
+         */
         internal void Reload(Rainmeter.API rm, ref double maxValue)
         {
-            // cache type settings from before laste Reload
+            ErrorString = rm.ReadString("ErrorString", "oops.");
+            // cache type settings from last Reload
             Dictionary<DriveType, bool> oldSettings = new Dictionary<DriveType, bool>(listedTypes);
-            // set type settings
+            // set type settings in dictionary
             listedTypes[DriveType.Fixed] = (rm.ReadInt("Fixed", 1) == 1 ? true : false);
-            listedTypes[DriveType.Removable] = (rm.ReadInt("Removable", 0) == 1 ? true : false);
-            listedTypes[DriveType.Network] = (rm.ReadInt("Network", 0) == 1 ? true : false);
+            listedTypes[DriveType.Removable] = (rm.ReadInt("Removable", 1) == 1 ? true : false);
+            listedTypes[DriveType.Network] = (rm.ReadInt("Network", 1) == 1 ? true : false);
             listedTypes[DriveType.CDRom] = (rm.ReadInt("Optical", 0) == 1 ? true : false);
             listedTypes[DriveType.Ram] = (rm.ReadInt("Ram", 0) == 1 ? true : false);
             // if any of the settings changes, then flag for a list update
-            if (!oldSettings.Equals(listedTypes)) needUpdate = true;
-
+            if (!oldSettings.Equals(listedTypes)) 
+                needUpdate = true;
         }
 
+        /* Runs every update cycle.  Checks connected to drives w/ GetDrives, and
+         * sets the update flag to true if the DriveInfo array from this update differs
+         * form that of the last update.
+         * updateListedDrives() is called if the update flag is set.
+         * Returns the number of items in listedDrives.
+         */
         internal double Update()
         {
             DriveInfo[] newDrives = DriveInfo.GetDrives();  // get array of DriveInfo objects
@@ -61,13 +97,16 @@ namespace PluginDriveList
             // if the flag is set, run an update.  
             if (needUpdate) 
                 updateListedDrives();
-
+            // if the drive list is empty (error, or settings that exclude all types) then log that
             if (listedDrives.Count < 1)
-                API.Log(API.LogType.Error, "DriveList: no drives?!");
-
+                API.Log(API.LogType.Warning, "DriveList: no drives listed");
+            // return the number of drives in the list
             return (double)listedDrives.Count;
         }
 
+        /* Returns the drive letter of the drive at CurrentIndex in listedDrives,
+         * or ErrorString is CurrentIndex is out of bounds (or other error).
+         */
         internal string GetString()
         {
             string t;
@@ -78,13 +117,17 @@ namespace PluginDriveList
             catch
             {
 #if DEBUG
-                API.Log(API.LogType.Warning, "Caught an IndexOOB exception");
+                API.Log(API.LogType.Warning, "DriveList: caught exception in GetString");
 #endif
                 t = ErrorString;
             }
             return t;
         }
 
+        /* Accepts "!CommandMeasure" bangs w/ arguments "forward"
+         * to move the current index up and "backward" to
+         * move the current index down.
+         */
         internal void ExecuteBang(string args)
         {
             switch (args.ToLowerInvariant())
@@ -101,35 +144,30 @@ namespace PluginDriveList
             }
         }
 
-        /* Clears and repopulates the list of drive names.
+        /* Clears and repopulates the list of drive names, 
+         * and resets the update flag.
          */
         private void updateListedDrives()
         {
             listedDrives.Clear();
             foreach (DriveInfo d in allDrives)
             {
-#if DEBUG
-                API.Log(API.LogType.Debug, "Found drive " + d.Name);
-#endif
                 if (d.IsReady && listedTypes[d.DriveType])
-                {
-                    listedDrives.Add(d.Name);
-#if DEBUG
-                    API.Log(API.LogType.Debug, "Added drive " + d.Name);
-#endif
-                }
+                    listedDrives.Add(d.Name.Substring(0, 2));
             }
-
+            // make sure the index is not out of bounds.  Will set the index to -1 if there are no drives in the list.
             if (currentIndex >= listedDrives.Count)
-            {
                 currentIndex = listedDrives.Count - 1;
-            }
-
-            needUpdate = false; // reset the update flag
+            // reset the update flag
+            needUpdate = false;
         }
 
     }
 
+    /* Binds the Measure class to the low-level Rainmeter C-based API.
+     * Rainmeter calls methods in the static plugin class w/ a measure id,
+     * and the Plugin class calls the methods of the correct Measure instance.
+     */
     public static class Plugin
     {
         [DllExport]
@@ -174,6 +212,7 @@ namespace PluginDriveList
             Measures[id].ExecuteBang(new string(args));
         }
 
+        // Maps measure IDs to Measure objects
         internal static Dictionary<uint, Measure> Measures = new Dictionary<uint, Measure>();
     }
 }
--- README.md
+++ README.md
@@ -1,14 +1,18 @@
 DriveList Plugin
-===================
+----------------
 Works with the [Rainmeter Plugin API](https://github.com/rainmeter/rainmeter-plugin-sdk "rainmeter-plugin-sdk")
 
-Simple C# plugin for Rainmeter that creates a list of drive letters for all currently mounted drives.  A DriveList plugin measure returns a String value of the "current" drive in the form "C:\", so you can use the value of the plugin measure as the "Drive" setting in a FreeDiskSpace measure (with DynamicVariables=1).
+Simple C# plugin for Rainmeter that creates a list of drive letters for all currently mounted drives.  A DriveList plugin measure returns a String value of the "current" drive in the form "C:", so you can use the value of the plugin measure as the "Drive" setting in a FreeDiskSpace measure (with DynamicVariables=1).
 
-##### Options 
-Set to 1 or 0 to change the drive types to include in the list
-* `Network`   - include the letters of mapped network drives
-* `Removable` - include the letters of mounted removable drives
-* `Optical`   - include the letters of mounted optical "CDRom" type drives
+##### Options
+* `ErrorString` - the plugin returns this string on error or before the drive list is populated.
+
+Types of drives to include in the list.  Set to 1 to include, or 0 to exclude.  `Fixed`, `Removable`, and `Network` are included by default.
+* `Fixed`		- Regular ol' hard drives
+* `Network`		- mapped network drives
+* `Removable`	- mounted removable drives
+* `Optical`		- mounted optical "CDRom" type drives
+* `Ram`			- RAM disks
 
 ##### Bangs
 Cycle through the list of drive letters
@@ -16,21 +20,21 @@ Cycle through the list of drive letters
 * `[!CommandMeasure "yourDriveListMeasure" "Backward"]`
  
 ##### Issues / To-do:
-- [ ] Error codes?
 - [ ] MOAR TESTING
-- [ ] Switching to a Network drive seems oddly laggy
 - [ ] Measure does not immediately return a string value; it appears that Measure.GetString() is actually called before Reload() and Update() are done populating the list if drives.
+- [ ] Does not return empty optical drives
  
-##### Simple example skin
+##### Example Skin
 ```
 [Rainmeter]
 Update=1000
+DynamicWindowSize=1
 
 [measureDriveList]
 Measure=PLUGIN
 Plugin=DriveList.dll
+ErrorString="C:"
 Optical=1
-Removable=1
 Network=0
 UpdateDivider=20
 
@@ -39,15 +43,15 @@ Measure=FREEDISKSPACE
 Drive=[measureDriveList]
 Label=1
 DynamicVariables=1
-UpdateDivider=10
-Group=DiskMsrs
+UpdateDivider=-1
+Group=fdsGroup
 
 [measureDiskSpace]
 Measure=FREEDISKSPACE
 Drive=[measureDriveList]
 DynamicVariables=1
-UpdateDivider=10
-Group=DiskMsrs
+UpdateDivider=60
+Group=fdsGroup
 
 [meterDriveName]
 Meter=STRING
@@ -59,19 +63,19 @@ AntiAlias=1
 SolidColor=0,0,0,200
 X=0
 Y=0
-Text="[measureDriveList] %1"
+Text="[measureDriveList]\ %1"
 DynamicVariables=1
-LeftMouseUpAction=[!CommandMeasure "measureDriveList" "forward"][!UpdateMeasureGroup DiskMsrs][!Redraw]
-RightMouseUpAction=[!CommandMeasure "measureDriveList" "backward"][!UpdateMeasureGroup DiskMsrs][!Redraw]
+LeftMouseUpAction=[!CommandMeasure "measureDriveList" "forward"][!UpdateMeasureGroup fdsGroup][!Update *][!Redraw]
+RightMouseUpAction=[!CommandMeasure "measureDriveList" "backward"][!UpdateMeasureGroup fdsGroup][!Update *][!Redraw]
 
 [meterDriveSpace]
 Meter=STRING
 MeterStyle=meterDriveName
 MeasureName=measureDiskSpace
 AutoScale=1
-X=r
-Y=R
-Text=%1
+X=R
+Y=r
+Text="%1 free"
 ```
 
 
